4fe34c46fffe03fb33eb536497903fd5d0f9ba36
Mehmet Genc <mehmetgencv@gmail.com>
2024-07-17 23:12:55 +0300

    add login

--- app/authentication.py
+++ app/authentication.py
@@ -13,7 +13,7 @@ def get_hashed_password(password):
     return password_context.hash(password)
 
 
-async def verify_token(token:str):
+async def verify_token(token: str):
     try:
         payload = jwt.decode(token, config_credentials["SECRET"], algorithms=["HS256"])
         user = await User.get(id=payload.get("id"))
@@ -22,3 +22,32 @@ async def verify_token(token:str):
                             detail="Invalid token",
                             headers={"WWW-Authenticate": "Bearer"})
     return user
+
+
+async def verify_password(plain_password: str, hashed_password: str):
+    return password_context.verify(plain_password, hashed_password)
+
+
+async def authenticate_user(username: str, password: str):
+    user = await User.get(username=username)
+
+    if user and verify_password(password, user.password):
+        return user
+    return False
+
+
+async def token_generator(username: str, password: str):
+    user = await authenticate_user(username, password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"})
+
+    token_data = {
+        "id": user.id,
+        "username": user.username,
+    }
+
+    token = jwt.encode(token_data, config_credentials["SECRET"], algorithm="HS256")
+    return token
--- main.py
+++ main.py
@@ -1,25 +1,67 @@
-from urllib.request import Request
-
-from fastapi import FastAPI, Request, HTTPException, status
-from tortoise.contrib.fastapi import register_tortoise
 from app.models import *
+from app.email import *
 from app.authentication import *
 
+from fastapi import FastAPI, Request, HTTPException, status, Depends
+from tortoise.contrib.fastapi import register_tortoise
+
+# authentication
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+# signals
 from tortoise.signals import post_save
 from typing import List, Optional, Type
 from tortoise import BaseDBAsyncClient
 
+# response classes
 from fastapi.responses import HTMLResponse
 
+# templates
 from fastapi.templating import Jinja2Templates
-from app.email import *
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 @app.get("/")
 async def index():
     return {"message": "Hello World"}
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, config_credentials["SECRET"], algorithms=["HS256"])
+        user = await User.get(id=payload.get("id"))
+        return await user
+
+    except:
+        HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"})
+
+
+@app.post("/token")
+async def generate_token(request_form: OAuth2PasswordRequestForm = Depends()):
+    token = await token_generator(request_form.username, request_form.password)
+    return {"access_token": token, "token_type": "bearer"}
+
+
+@app.post("/user/me")
+async def user_login(user: user_pydanticIn = Depends(get_current_user)):
+    business = await Business.get(owner=user)
+    return {
+        "status": "success",
+        "data": {
+            "username": user.username,
+            "email": user.email,
+            "verified": user.is_verified,
+            "join_date": user.join_date.strftime("%m/%d/%Y"),
+
+        }
+    }
 
 
 @app.post("/registration")
@@ -35,7 +77,6 @@ async def user_registrations(user: user_pydanticIn):
     }
 
 
-templates = Jinja2Templates(directory="templates")
 @app.get("/verification", response_class=HTMLResponse)
 async def email_verification(request: Request, token: str):
     user = await verify_token(token)
@@ -56,7 +97,7 @@ async def create_business(
         created: bool,
         using_db: "Optional[BaseDBAsyncClient]",
         update_fields: List[str],
-)-> None:
+) -> None:
     if created:
         business_obj = await Business.create(
             business_name=instance.username,
@@ -64,6 +105,7 @@ async def create_business(
         )
         await business_pydantic.from_tortoise_orm(business_obj)
         await send_email([instance.email], instance)
+
 
 register_tortoise(
     app,
