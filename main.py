136d80944cd79ef29910104d5b32f84921b0aace
Mehmet Genc <mehmetgencv@gmail.com>
2024-07-27 13:54:36 +0300

    add Profile Pictures and Product Pictures

--- main.py
+++ main.py
@@ -19,10 +19,19 @@ from fastapi.responses import HTMLResponse
 # templates
 from fastapi.templating import Jinja2Templates
 
+# image upload
+from fastapi import File, UploadFile
+import secrets
+from fastapi.staticfiles import StaticFiles
+from PIL import Image
+
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+# static file setup config
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/")
@@ -105,6 +114,85 @@ async def create_business(
         )
         await business_pydantic.from_tortoise_orm(business_obj)
         await send_email([instance.email], instance)
+
+
+@app.post("/uploadfile/profile")
+async def create_upload_file(file: UploadFile = File(...),
+                             user: user_pydantic = Depends(get_current_user)):
+    FILEPATH = "./static/ímages"
+    file_name = file.filename
+    extension = file_name.split(".")[-1]
+
+    if extension not in ["png", "jpg", "jpeg"]:
+        return {"status": "error", "detail": "File extension is not allowed."}
+
+    token_name = secrets.token_hex(10) + "." + extension
+    generated_name = FILEPATH + token_name
+    file_content = await file.read()
+    with open(generated_name, "wb") as file:
+        file.write(file_content)
+
+    img = Image.open(generated_name)
+    img = img.resize(size=(200, 200))
+    img.save(generated_name)
+
+    file.close()
+    business = await Business.get(owner=user)
+    owner = await business.owner
+
+    if not owner == user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Not authorized to perform this action.",
+                            headers={"WWW-Authenticate": "Bearer"})
+
+    business.logo = token_name
+    await business.save()
+
+    file_url = "http://localhost:8000" + generated_name[1:]
+    return {
+        "status": "success",
+        "filename": file_url
+    }
+
+@app.post("/uploadfile/product/{id}")
+async def create_update_file(id: int, file: UploadFile = File(...),
+                             user: user_pydantic = Depends(get_current_user)):
+
+    FILEPATH = "./static/ímages"
+    file_name = file.filename
+    extension = file_name.split(".")[-1]
+
+    if extension not in ["png", "jpg", "jpeg"]:
+        return {"status": "error", "detail": "File extension is not allowed."}
+
+    token_name = secrets.token_hex(10) + "." + extension
+    generated_name = FILEPATH + token_name
+    file_content = await file.read()
+    with open(generated_name, "wb") as file:
+        file.write(file_content)
+
+    img = Image.open(generated_name)
+    img = img.resize(size=(200, 200))
+    img.save(generated_name)
+
+    file.close()
+    product = await Product.get(id=id)
+    business = await product.business
+    owner = await business.owner
+
+    if not owner == user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Not authorized to perform this action.",
+                            headers={"WWW-Authenticate": "Bearer"})
+
+    product.product_image = token_name
+    await product.save()
+
+    file_url = "http://localhost:8000" + generated_name[1:]
+    return {
+        "status": "success",
+        "filename": file_url
+    }
 
 
 register_tortoise(
